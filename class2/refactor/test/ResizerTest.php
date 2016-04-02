d9962833b827ce34553ba2626444d0d60ad1654e
jeslopcru <jeslopcru@gmail.com>
2016-04-02 10:23:13 +0200

    file not exists

--- class2/refactor/test/ResizerTest.php
+++ class2/refactor/test/ResizerTest.php
@@ -66,24 +66,24 @@ class ResizerTest extends PHPUnit_Framework_TestCase {
         $resizer = new Resizer(new ImagePath('http://martinfowler.com/mf.jpg?query=hello&s=fowler'));
     }
 
-//    /**
-//     * @expectedException RuntimeException
-//     */
-//    public function testFilePathNotExists() {
-//        $configuration = new Configuration(array('width' => 800, 'height' => 600));
-//        $imagePath = new ImagePath('http://martinfowler.com/mf.jpg?query=hello&s=fowler');
-//        $resizer = new Resizer($imagePath, $configuration);
-//
-//        $stub = $this->getMockBuilder('FileSystem')
-//            ->getMock();
-//        $stub->method('file_exists')
-//            ->willReturn(true);
-//
-//        $stub->method('filemtime')
-//            ->willReturn(21 * 60);
-//
-//        $resizer->injectFileSystem($stub);
-//
-//        $this->assertEquals('./cache/remote/mf.jpg', $resizer->obtainFilePath());    }
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testFilePathNotExists() {
+        $configuration = new Configuration(array('width' => 800, 'height' => 600));
+        $imagePath = new ImagePath('http://martinfowler.com/mf.jpg?query=hello&s=fowler');
+        $resizer = new Resizer($imagePath, $configuration);
+
+        $stub = $this->getMockBuilder('FileSystem')
+            ->getMock();
+        $stub->method('file_exists')
+            ->willReturn(false);
+
+        $stub->method('filemtime')
+            ->willReturn(21 * 60);
+
+        $resizer->injectFileSystem($stub);
+
+        $this->assertEquals('./cache/remote/mf.jpg', $resizer->obtainFilePath());    }
 
 }
