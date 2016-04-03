975c2d86f65db4024ecc024e8a339e37444d399d
jeslopcru <jeslopcru@gmail.com>
2016-04-03 13:35:18 +0200

    refactor resize

--- class2/refactor/ShellCommand.php
+++ class2/refactor/ShellCommand.php
@@ -4,67 +4,87 @@
 class ShellCommand
 {
     private $configuration;
+    private $fileSystem;
 
     public function __construct(Configuration $configuration)
     {
         $this->configuration = $configuration;
+        $this->fileSystem = new FileSystem();
+
     }
-    
-    public function defaultShellCommand($imagePath, $newPath) {
+
+    public function defaultShellCommand($imagePath, $newPath)
+    {
         $w = $this->configuration->obtainWidth();
         $h = $this->configuration->obtainHeight();
 
-        $command = $this->configuration->obtainConvertPath() ." " . escapeshellarg($imagePath) .
-            " -thumbnail ". (!empty($h) ? 'x':'') . $w ."".
-            ($this->configuration->obtainMaxOnly()== true ? "\>" : "") .
-            " -quality ". escapeshellarg($this->configuration->obtainQuality()) ." ". escapeshellarg($newPath);
+        $command = $this->configuration->obtainConvertPath() . " " . escapeshellarg($imagePath) .
+            " -thumbnail " . (!empty($h) ? 'x' : '') . $w . "" .
+            ($this->configuration->obtainMaxOnly() == true ? "\>" : "") .
+            " -quality " . escapeshellarg($this->configuration->obtainQuality()) . " " . escapeshellarg($newPath);
 
         return $command;
     }
 
-    public function commandWithScale($imagePath, $newPath) {
+    public function commandWithScale($imagePath, $newPath)
+    {
         $resize = $this->composeResizeOptions($imagePath, $this->configuration);
 
-        $cmd = $this->configuration->obtainConvertPath() ." ". escapeshellarg($imagePath) ." -resize ". escapeshellarg($resize) .
-            " -quality ". escapeshellarg($this->configuration->obtainQuality()) . " " . escapeshellarg($newPath);
+        $cmd = $this->configuration->obtainConvertPath() . " " . escapeshellarg($imagePath) . " -resize " . escapeshellarg($resize) .
+            " -quality " . escapeshellarg($this->configuration->obtainQuality()) . " " . escapeshellarg($newPath);
 
         return $cmd;
     }
 
-    public function commandWithCrop($imagePath, $newPath) {
+    public function composeResizeOptions($imagePath)
+    {
         $w = $this->configuration->obtainWidth();
         $h = $this->configuration->obtainHeight();
-        $resize = $this->composeResizeOptions($imagePath, $this->configuration);
 
-        $cmd = $this->configuration->obtainConvertPath() ." ". escapeshellarg($imagePath) ." -resize ". escapeshellarg($resize) .
-            " -size ". escapeshellarg($w ."x". $h) .
-            " xc:". escapeshellarg($this->configuration->obtainCanvasColor()) .
-            " +swap -gravity center -composite -quality ". escapeshellarg($this->configuration->obtainQuality())." ".escapeshellarg($newPath);
+        $resize = "x" . $h;
 
-        return $cmd;
+        if ($this->needResize($this->hasCrop(), $this->isPanoramic($imagePath))) {
+            $resize = $w;
+
+        }
+
+        return $resize;
     }
 
-    function isPanoramic($imagePath) {
-        list($width,$height) = getimagesize($imagePath);
+    private function hasCrop()
+    {
+        return (true === $this->configuration->obtainCrop());
+    }
+
+    private function needResize($hasCrop, $isPanoramicImage)
+    {
+        return
+            (!$hasCrop && $isPanoramicImage) ||
+            ($hasCrop && !$isPanoramicImage);
+    }
+
+    public function isPanoramic($imagePath)
+    {
+        list($width, $height) = $this->fileSystem->getimagesize($imagePath);
         return $width > $height;
     }
 
-    function composeResizeOptions($imagePath) {
+    public function commandWithCrop($imagePath, $newPath)
+    {
         $w = $this->configuration->obtainWidth();
         $h = $this->configuration->obtainHeight();
+        $resize = $this->composeResizeOptions($imagePath, $this->configuration);
 
-        $resize = "x".$h;
+        $cmd = $this->configuration->obtainConvertPath() . " " . escapeshellarg($imagePath) . " -resize " . escapeshellarg($resize) .
+            " -size " . escapeshellarg($w . "x" . $h) .
+            " xc:" . escapeshellarg($this->configuration->obtainCanvasColor()) .
+            " +swap -gravity center -composite -quality " . escapeshellarg($this->configuration->obtainQuality()) . " " . escapeshellarg($newPath);
 
-        $hasCrop = (true === $this->configuration->obtainCrop());
+        return $cmd;
+    }
 
-        if(!$hasCrop && $this->isPanoramic($imagePath)):
-            $resize = $w;
-        endif;
-
-        if($hasCrop && !$this->isPanoramic($imagePath)):
-            $resize = $w;
-        endif;
-
-        return $resize;
+    public function injectFileSystem(FileSystem $fileSystem)
+    {
+        $this->fileSystem = $fileSystem;
     }
 }
--- class2/refactor/test/ShellCommandTest.php
+++ class2/refactor/test/ShellCommandTest.php
@@ -45,29 +45,132 @@ class ShellCommandTest extends PHPUnit_Framework_TestCase
     }
 
 
-    public function testIsPanoramic()
+    public function testIsNotPanoramicImage()
     {
         $shell = new ShellCommand(new Configuration());
-        $this->assertFalse($shell->isPanoramic($this->pathToRealImage));
+
+        $stub = $this->obtainANotPanoramicImageMock();
+
+        $shell->injectFileSystem($stub);
+
+        $isPanoramic = $shell->isPanoramic($this->pathToRealImage);
+        $this->assertFalse($isPanoramic);
     }
 
-    public function testComposeResizeOptions()
+    private function obtainANotPanoramicImageMock()
+    {
+        $stub = $this->getMockBuilder('FileSystem')
+            ->getMock();
+        $stub->expects($this->exactly(1))
+            ->method('getimagesize')->will($this->returnValue([1, 2]));
+        return $stub;
+    }
+
+    public function testIsAPanoramicImage()
+    {
+        $shell = new ShellCommand(new Configuration());
+
+        $stub = $this->obtainAPanoramicImageMock();
+        $shell->injectFileSystem($stub);
+
+        $this->assertTrue($shell->isPanoramic($this->pathToRealImage));
+    }
+
+    private function obtainAPanoramicImageMock()
+    {
+        $stub = $this->getMockBuilder('FileSystem')
+            ->getMock();
+        $stub->expects($this->exactly(1))
+            ->method('getimagesize')->will($this->returnValue([2, 1]));
+        return $stub;
+    }
+
+    public function testResizeNotPanoramicImageWithWidth()
     {
         $configuration = new Configuration([Configuration::WIDTH_KEY => 66,]);
         $shell = new ShellCommand($configuration);
 
-        $this->assertEquals('x', $shell->composeResizeOptions($this->pathToRealImage));
+        $stub = $this->obtainANotPanoramicImageMock();
+        $shell->injectFileSystem($stub);
 
-        $configuration = new Configuration([Configuration::WIDTH_KEY => 4, Configuration::CROP_KEY => true]);
+        $this->assertEquals('x', $shell->composeResizeOptions($this->pathToRealImage));
+    }
+
+    public function testResizePanoramicImageWithWidth()
+    {
+        $expectedResult = 66;
+        $configuration = new Configuration([Configuration::WIDTH_KEY => $expectedResult,]);
         $shell = new ShellCommand($configuration);
 
-        $this->assertEquals('4', $shell->composeResizeOptions($this->pathToRealImage));
+        $stub = $this->obtainAPanoramicImageMock();
+        $shell->injectFileSystem($stub);
 
+        $this->assertEquals($expectedResult, $shell->composeResizeOptions($this->pathToRealImage));
+    }
+
+    public function testResizePanoramicImageWithWidthAndCrop()
+    {
+        $configuration = new Configuration([Configuration::WIDTH_KEY => 665, Configuration::CROP_KEY => true]);
+        $shell = new ShellCommand($configuration);
+
+        $stub = $this->obtainAPanoramicImageMock();
+        $shell->injectFileSystem($stub);
+
+        $this->assertEquals('x', $shell->composeResizeOptions($this->pathToRealImage));
+    }
+
+    public function testResizeNotPanoramicImageWithWidthAndCrop()
+    {
+        $expectedResult = 45;
+        $configuration = new Configuration([
+            Configuration::WIDTH_KEY => $expectedResult,
+            Configuration::CROP_KEY => true
+        ]);
+        $shell = new ShellCommand($configuration);
+
+        $stub = $this->obtainANotPanoramicImageMock();
+        $shell->injectFileSystem($stub);
+
+        $this->assertEquals($expectedResult, $shell->composeResizeOptions($this->pathToRealImage));
+    }
+
+
+    public function testResizeNotPanoramicImageWithHeight()
+    {
         $configuration = new Configuration([Configuration::HEIGHT_KEY => 4,]);
         $shell = new ShellCommand($configuration);
 
-        $this->assertEquals('x4', $shell->composeResizeOptions($this->pathToRealImage));
+        $stub = $this->obtainANotPanoramicImageMock();
+        $shell->injectFileSystem($stub);
 
+        $this->assertEquals('x4', $shell->composeResizeOptions($this->pathToRealImage));
+    }
+
+    public function testResizeNotPanoramicImageWithHeightAndWidth()
+    {
+        $configuration = new Configuration([Configuration::HEIGHT_KEY => 4,Configuration::WIDTH_KEY => 5,]);
+        $shell = new ShellCommand($configuration);
+
+        $stub = $this->obtainANotPanoramicImageMock();
+        $shell->injectFileSystem($stub);
+
+        $this->assertEquals('x4', $shell->composeResizeOptions($this->pathToRealImage));
+    }
+
+    public function testResizePanoramicImageWithHeightAndWidth()
+    {
+        $expectedResult = 5;
+        $configuration = new Configuration([Configuration::HEIGHT_KEY => 4,Configuration::WIDTH_KEY => $expectedResult,]);
+        $shell = new ShellCommand($configuration);
+
+        $stub = $this->obtainAPanoramicImageMock();
+        $shell->injectFileSystem($stub);
+
+        $this->assertEquals($expectedResult, $shell->composeResizeOptions($this->pathToRealImage));
+    }
+
+    public function testComposeResizeOptions()
+    {
         $configuration = new Configuration([Configuration::HEIGHT_KEY => 4, Configuration::CROP_KEY => true]);
         $shell = new ShellCommand($configuration);
         $this->assertEquals(null, $shell->composeResizeOptions($this->pathToRealImage));
