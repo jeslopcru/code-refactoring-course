678dea7e4a49a26f68adeb50300887634577eafd
jeslopcru <jeslopcru@gmail.com>
2016-04-04 21:46:50 +0200

    rename

--- class2/refactor/autoload.php
+++ class2/refactor/autoload.php
@@ -6,6 +6,7 @@ require_once 'Resizer.php';
 require_once 'ImagePath.php';
 require_once 'ResizerOptions.php';
 require_once 'UrlImage.php';
+require_once 'Path.php';
 
 require_once 'function.resize.php';
 
