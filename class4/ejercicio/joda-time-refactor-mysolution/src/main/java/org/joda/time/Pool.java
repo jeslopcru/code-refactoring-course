bdf5c2ae7d6906facbdb88d538fb811faf94d9d0
jeslopcru <jeslopcru@gmail.com>
2016-04-22 19:43:14 +0200

    hours en el pool

--- class4/ejercicio/joda-time-refactor-mysolution/src/main/java/org/joda/time/Pool.java
+++ class4/ejercicio/joda-time-refactor-mysolution/src/main/java/org/joda/time/Pool.java
@@ -6,11 +6,14 @@ public class Pool {
 
     private static Pool myInstance;
     private HashMap<Integer, Days> days;
+    private HashMap<Integer, Hours> hours;
     private HashMap<Integer, Minutes> minutes;
 
     private Pool() {
         this.days = new HashMap<Integer, Days>();
+        this.hours = new HashMap<Integer, Hours>();
         this.minutes = new HashMap<Integer, Minutes>();
+
     }
 
     public static Pool getInstance() {
@@ -22,55 +25,66 @@ public class Pool {
         return myInstance;
     }
 
-    private void addDay(int numeral, Days day) {
-        days.put(new Integer(numeral), day);
-    }
-
-
-
     public static Days retrieveDays(int numeral) {
         Pool pool = Pool.getInstance();
 
-        Object result = pool.getDays(numeral);
+        Days result = pool.getDays(numeral);
 
         if (result == null) {
-            result =  new Days(numeral);
-            pool.addDay(numeral, (Days) result);
+            result = new Days(numeral);
+            pool.addDay(numeral, result);
         }
 
-        return (Days) result;
+        return result;
     }
-
 
     public static Minutes retrieveMinutes(int numeral) {
 
         Pool pool = Pool.getInstance();
 
-        Object result = pool.getMinutes(numeral);
+        Minutes result = pool.getMinutes(numeral);
 
         if (result == null) {
-            result =  new Minutes(numeral);
-            pool.addMinutes(numeral, (Minutes) result);
+            result = new Minutes(numeral);
+            pool.addMinutes(numeral, result);
         }
 
-        return (Minutes) result;
+        return result;
+    }
+
+    public static Hours retrieveHours(int i) {
+        Pool pool = Pool.getInstance();
+
+        Hours result = pool.getHours(i);
+
+        if (result == null) {
+            result = new Hours(i);
+            pool.addHours(i, result);
+        }
+        return result;
+    }
+
+    private void addDay(int numeral, Days day) {
+        days.put(numeral, day);
+    }
+
+    private void addHours(int i, Hours result) {
+        hours.put(i, result);
     }
 
     private void addMinutes(int numeral, Minutes minute) {
-        minutes.put(new Integer(numeral), minute);
+        minutes.put(numeral, minute);
     }
 
-
-    private Object getDays(int numeral){
-        Object instance = days.get(new Integer(numeral));
-
-        return instance;
-
+    private Days getDays(int numeral) {
+        return days.get(numeral);
     }
 
-    private Object getMinutes(int numeral) {
-        Object instance = minutes.get(new Integer(numeral));
+    private Hours getHours(int i) {
+        return hours.get(i);
+    }
 
-        return instance;
+    private Minutes getMinutes(int numeral) {
+        return minutes.get(numeral);
     }
 }
